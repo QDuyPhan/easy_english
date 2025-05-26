cb8e96b3a706909a677afc1de1ab85b6dbb97c28
Quang Duy <Phanquangduytvt.work@gmail.com>
2025-05-26 10:42:38 +0700

    Setup App Color
    This commit introduces the `AppColor` class, which defines a set of constant color values for the application.
    
    The color palette is organized into the following categories:
    - Primary Color
    - Secondary Color
    - Base Color
    - Accent Color (Success, Warning, Error, Info)

---
+++ lib/core/config/app_color.dart
@@ -0,0 +1,52 @@
+import 'dart:ui';
+
+class AppColor {
+  const AppColor._();
+
+  //Primary Color
+  static const Color primary100 = Color(0xFF0074CE);
+  static const Color primary80 = Color(0xFF0693F1);
+  static const Color primary60 = Color(0xFF3DB2FF);
+  static const Color primary40 = Color(0xFFB8E1FF);
+  static const Color primary20 = Color(0xFFEFF8FF);
+
+  //Secondary Color
+  static const Color secondary100 = Color(0xFFFFC107);
+  static const Color secondary80 = Color(0xFFFFD899);
+  static const Color secondary60 = Color(0xFFFFE5BB);
+  static const Color secondary40 = Color(0xFFFFF2DD);
+  static const Color secondary20 = Color(0xFFFFF9EF);
+
+  //Base Color
+  static const Color black100 = Color(0xFF2C3131);
+  static const Color black80 = Color(0xFF626262);
+  static const Color black60 = Color(0xFFA9A9A9);
+  static const Color black40 = Color(0xFFCACACA);
+  static const Color black20 = Color(0xFFE9E9E9);
+  static const Color black0 = Color(0xFFFFFFFF);
+
+  //Accent Color
+  static const Color success100 = Color(0xFF3F845F);
+  static const Color success80 = Color(0xFF659D7F);
+  static const Color success60 = Color(0xFF8CB59F);
+  static const Color success40 = Color(0xFFB2CEBF);
+  static const Color success20 = Color(0xFFD9E6DF);
+
+  static const Color warning100 = Color(0xFFE4C65B);
+  static const Color warning80 = Color(0xFFE9D17C);
+  static const Color warning60 = Color(0xFFEFDD9D);
+  static const Color warning40 = Color(0xFFF4E8BD);
+  static const Color warning20 = Color(0xFFFAF4DE);
+
+  static const Color error100 = Color(0xFFE25C5C);
+  static const Color error80 = Color(0xFFE87D7D);
+  static const Color error60 = Color(0xFFEE9D9D);
+  static const Color error40 = Color(0xFFF3BEBE);
+  static const Color error20 = Color(0xFFF9DEDE);
+
+  static const Color info100 = Color(0xFF2685CA);
+  static const Color info80 = Color(0xFF519DD5);
+  static const Color info60 = Color(0xFF7DB6DF);
+  static const Color info40 = Color(0xFFA8CEEA);
+  static const Color info20 = Color(0xFFD4E7F4);
+}
